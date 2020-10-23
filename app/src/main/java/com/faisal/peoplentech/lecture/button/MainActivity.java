e29136a86ae0ecde8c9b2154b6866980d5ddb090
Nirzon Pop <nirzon192@gmail.com>
2020-10-23 12:38:44 +0600

    Initial commit

---
+++ app/src/main/java/com/faisal/peoplentech/lecture/button/MainActivity.java
@@ -0,0 +1,18 @@
+package com.faisal.peoplentech.lecture.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * @see  {@link {on stack oveflow}https://stackoverflow.com/questions/6054562/how-to-make-the-corners-of-a-button-round}
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
